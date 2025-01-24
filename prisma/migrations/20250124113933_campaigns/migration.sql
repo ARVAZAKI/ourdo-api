9fc0b0191f27bfca6117d0c7034e17d12fa77d1a
ARVAZAKI <arvazf09@gmail.com>
2025-01-24 18:44:03 +0700

    setup database version 1

---
+++ prisma/migrations/20250124113933_campaigns/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Campaign" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "donation_target" BIGINT NOT NULL,
+    "category" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Campaign_pkey" PRIMARY KEY ("id")
+);
